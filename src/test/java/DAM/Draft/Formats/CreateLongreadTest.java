3904df62557e11bc459d0a0244659daf249ab966
skamyn <skamynin@gmail.com>
2022-04-05 13:42:56 +0300

    Добавлено создание шаблона Лонгрида

---
+++ src/test/java/DAM/Draft/Formats/CreateLongreadTest.java
@@ -0,0 +1,32 @@
+package DAM.Draft.Formats;
+
+import DAM.LogIn;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateLongreadTest {
+    String logIn;
+
+    @Before
+    public void getEndpoint() throws JSONException {
+        logIn = new LogIn().logIn();
+    }
+
+    @Test
+    public void successCreate() {
+        given()
+                .cookie("JSESSIONID", logIn)
+                .contentType("multipart/form-data")
+                .multiPart("thumbnail", new File("C:\\Users\\skamy\\Desktop\\Picture\\33.png"),
+                        "image/png")
+                .multiPart("template", new File("C:\\Users\\skamy\\Desktop\\yaml\\json\\template\\" +
+                                "Longread_JSON.json"), "application/json")
+                .when()
+                .post("/templates");
+    }
+}
